8343e8c5e79bde4b270b592209d460f1b376250c
shannonbee <shannonbenyamin@gmail.com>
2018-07-10 16:36:50 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,5 +2,8 @@
 
 # Try starting out with puts'ing a string
 puts  "Hi! You've been invited to a party. What's your name"
-guest_name=gets.chomp
+guest_name = gets.chomp
+
+puts"What was the name of this party?"
+n
 

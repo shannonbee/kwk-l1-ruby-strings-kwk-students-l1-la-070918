9ee9693f9e632499014e8289426e58f810a0eae1
shannonbee <shannonbenyamin@gmail.com>
2018-07-10 16:29:08 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -3,6 +3,4 @@
 # Try starting out with puts'ing a string
 puts " Hi! You've been invited to a party. What's your name"
 name = gets
-puts "What is the name of your party"
-party = gets
-puts"What"
+
